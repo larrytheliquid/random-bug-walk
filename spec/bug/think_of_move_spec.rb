1ad825adf1516123a524e1182b2f2de408f0feaa
Larry Diehl <larry@izea.com>
2008-06-28 10:46:01 -0400

    Cleaned think_of_move_spec

--- spec/bug/think_of_move_spec.rb
+++ spec/bug/think_of_move_spec.rb
@@ -1,56 +1,56 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 module BugWalkSimulation  
-  describe Bug, "#think_of_move, with think_of_moves north, south, east, west, northeast, northwest, southeast, and southwest" do
+  describe Bug, "#think_of_move, with moves north, south, east, west, northeast, northwest, southeast, and southwest" do
     before(:each) do
       @bug = Factory.new_bug(:think_of_moves => [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]])
       @timeout = 3
       srand(1337)
     end
 
-    it "should be able to think_of_move north" do
+    it "should be able to think of move north" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [-1, 0] }      
       end.should_not raise_error
     end
 
-    it "should be able to think_of_move south" do
+    it "should be able to think of move south" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [1, 0] }      
       end.should_not raise_error    
     end
     
-    it "should be able to think_of_move west" do
+    it "should be able to think of move west" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [0, -1] }      
       end.should_not raise_error    
     end
 
-    it "should be able to think_of_move east" do
+    it "should be able to think of move east" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [0, 1] }      
       end.should_not raise_error    
     end
     
-    it "should be able to think_of_move northwest" do
+    it "should be able to think of move northwest" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [-1, -1] }      
       end.should_not raise_error    
     end
 
-    it "should be able to think_of_move northeast" do
+    it "should be able to think of move northeast" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [-1, 1] }      
       end.should_not raise_error    
     end
     
-    it "should be able to think_of_move southwest" do
+    it "should be able to think of move southwest" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [1, -1] }      
       end.should_not raise_error    
     end
 
-    it "should be able to think_of_move southeast" do
+    it "should be able to think of move southeast" do
       lambda do
         Timeout.timeout(@timeout) { think_of_move = @bug.think_of_move until think_of_move == [1, 1] }      
       end.should_not raise_error    
