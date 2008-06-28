bfd97d7014b1aafaf848a53ae8edcc366577eaf5
Larry Diehl <larry@izea.com>
2008-06-27 21:48:56 -0400

    Made tests for Bug#move deterministic

--- spec/bug/move_spec.rb
+++ spec/bug/move_spec.rb
@@ -3,37 +3,54 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Bug, "#move" do
   before(:each) do
     @bug = Factory.new_bug
+    @timeout = 3
   end
   
   it "should be able to move north" do
-    move = @bug.move until move == :north
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :north }      
+    end.should_not raise_error
   end
   
   it "should be able to move south" do
-    move = @bug.move until move == :south    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :south }      
+    end.should_not raise_error    
   end
   
   it "should be able to move east" do
-    move = @bug.move until move == :east    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :east }      
+    end.should_not raise_error    
   end
   
   it "should be able to move west" do
-    move = @bug.move until move == :west    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :west }      
+    end.should_not raise_error    
   end
   
   it "should be able to move northeast" do
-    move = @bug.move until move == :northeast    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :northeast }      
+    end.should_not raise_error    
   end
   
   it "should be able to move northwest" do
-    move = @bug.move until move == :northwest    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :northwest }      
+    end.should_not raise_error    
   end
   
   it "should be able to move southeast" do
-    move = @bug.move until move == :southeast    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :southeast }      
+    end.should_not raise_error    
   end
   
   it "should be able to move southwest" do
-    move = @bug.move until move == :southwest    
+    lambda do
+      Timeout.timeout(@timeout) { move = @bug.move until move == :southwest }      
+    end.should_not raise_error    
   end
 end
