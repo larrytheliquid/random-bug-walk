eceab12b2c2e3a998bc4eb05f956535dc9ba0ed4
Larry Diehl <larry@izea.com>
2008-06-27 22:51:35 -0400

    Added Floor#width and Floor#height

--- lib/bugr/floor.rb
+++ lib/bugr/floor.rb
@@ -4,4 +4,12 @@ class Floor
   def initialize(options = {})
     @tiles = Array.new options[:height], Array.new(options[:width], 0)
   end
+  
+  def height
+    tiles.size
+  end
+  
+  def width
+    tiles.first.size
+  end
 end
---
+++ spec/floor/height_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Floor, "#height" do
+  before(:each) do
+    @floor = Factory.new_floor(:height => 7)
+  end
+  
+  it "should be equal to the initialized height of the floor" do
+    @floor.height.should == 7
+  end
+end
